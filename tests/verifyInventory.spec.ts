082ff9fdca18b310dc0ebf8b04216c52be40464e
Adam Szurma <adam.szurma@gmail.com>
2024-07-31 21:08:39 +0200

    add .env to gitignore + quick inventory check

--- tests/verifyInventory.spec.ts
+++ tests/verifyInventory.spec.ts
@@ -2,5 +2,9 @@ const { test, expect } =require('./../utils/login.js')
 
 
 test('Verify inventory on page', async({inventoryPage}) => {
-await expect(inventoryPage.headingLabel).toContainText('Products');
+    await expect(inventoryPage.headingLabel).toContainText('Swag Labs');
+    await inventoryPage.backpackItem.containsText('Sauce Labs Backpackcarry.');
+    await inventoryPage.biklightItem.containsText('Sauce Labs Bike LightA red');
+    await inventoryPage.tshirtItem.containsText('Sauce Labs Bolt T-ShirtGet');
+    await inventoryPage.jacketItem.containsText('Sauce Labs Fleece JacketIt\'s');
 })
--- views/loginPage.view.ts
+++ views/loginPage.view.ts
@@ -10,12 +10,4 @@ export class LoginPage{
        this.passwordInput = page.locator('[data-test="password"]')
        this.loginButton = page.locator('[data-test="login-button"]')
     }
-    async goto(){
-        await this.page.goto('/');
-    }
-    async login(username: string, password: string){
-        await this.usernameInput.fill(username);
-        await this.passwordInput.fill(password);
-        await this.loginButton.click();
-    }
 }
